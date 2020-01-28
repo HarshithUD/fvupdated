5ab11a31c6fe62230b3ae8a446dfb036ebe939e0
Harshith R <harshdevil@Harshiths-MacBook-Air.local>
2020-01-28 15:11:50 +0530

    Added

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // require('dotenv').config()
 
 module.exports = {
-    mongoUri: 'mongodb+srv://harshith_12:5efiGd6zHlXnbPr1@queuereferral-tfxaw.mongodb.net/ReferralQueue2?retryWrites=true&w=majority',
+    mongoUri: 'mongodb+srv://harshith_12:5efiGd6zHlXnbPr1@queuereferral-tfxaw.mongodb.net/ReferralQueue?retryWrites=true&w=majority',
     secretOrKey: "secret"
 }
